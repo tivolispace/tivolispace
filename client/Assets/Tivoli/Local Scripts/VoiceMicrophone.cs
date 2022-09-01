9cc938cb6123e4fb6adb9631e32eab525cb94773
Maki <mxmcube@gmail.com>
2022-08-31 20:20:48 -0400

    Microphone will stop recording randomly

--- client/Assets/Tivoli/Local Scripts/VoiceMicrophone.cs
+++ client/Assets/Tivoli/Local Scripts/VoiceMicrophone.cs
@@ -10,16 +10,16 @@ namespace Tivoli.Local_Scripts
 
         public Action<float[]> OnPcmData;
 
-        public void StartMicrophone()
+        public void StartMicrophone(bool force = false)
         {
-            if (_microphone != null) return;
+            if (!force && _microphone != null) return;
             Debug.Log("Starting microphone");
             _microphone = Microphone.Start(null, true, 5, 44100);
         }
         
-        public void StopMicrophone()
+        public void StopMicrophone(bool force = false)
         {
-            if (_microphone == null) return;
+            if (!force && _microphone == null) return;
             Debug.Log("Stopping microphone");
             Microphone.End(null);
             _microphone = null;
@@ -27,6 +27,10 @@ namespace Tivoli.Local_Scripts
         
         private void Update()
         {
+            var isRecording = Microphone.IsRecording(null);
+            if (!isRecording && _microphone != null) StartMicrophone(true);
+            else if (isRecording && _microphone == null) StopMicrophone(true);
+            
             // send voice
             if ((_pos = Microphone.GetPosition(null)) > 0)
             {
