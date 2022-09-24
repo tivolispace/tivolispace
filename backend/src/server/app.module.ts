7e36c5ee7124dc5ae95bddcf2aff5797c25804d2
Maki <mxmcube@gmail.com>
2022-09-24 17:34:52 -0500

    Load database

--- backend/src/server/app.module.ts
+++ backend/src/server/app.module.ts
@@ -1,12 +1,28 @@
 import { Module } from "@nestjs/common";
 import { ViewModule } from "./view/view.module";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
 		}),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => ({
+				type: "postgres",
+				host: configService.get("dbHost", "localhost"),
+				port: configService.get("dbPort", 5432),
+				username: configService.get("dbUsername", "postgres"),
+				password: configService.get("dbPassword", "postgres"),
+				name: configService.get("dbName", "tivolispace"),
+				// synchronize: true, // dont use in production
+				entities: [],
+			}),
+		}),
+		// import view module last because it uses *
 		ViewModule,
 	],
 	controllers: [],
